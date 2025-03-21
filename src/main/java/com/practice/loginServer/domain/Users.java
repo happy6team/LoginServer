5cbddbb22e9ce75f74c593c0e1ba7c1ca5440da1
world-dv <goodforx0147@naver.com>
2025-03-21 09:00:49 +0900

    feat: user entity

---
+++ src/main/java/com/practice/loginServer/domain/Users.java
@@ -0,0 +1,31 @@
+package com.practice.loginServer.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+}
